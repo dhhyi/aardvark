27a77bb2e3e16038b64d2eba4930f548dbedcdf4
Danilo Hoffmann <dhhyi@aol.com>
2020-03-07 18:23:18 +0100

    generate reports with ui

--- 03_aardvark_map_data.groovy
+++ 03_aardvark_map_data.groovy
@@ -1,43 +1,44 @@
-def rules = new groovy.json.JsonSlurper().parseText new File('rules.json').text
-def config = new groovy.json.JsonSlurper().parseText(new File('config.json').text)
-
 @Grab('com.xlson.groovycsv:groovycsv:1.3')
 import static com.xlson.groovycsv.CsvParser.parseCsv
 
-def report = parseCsv(new File('report.csv').text, separator: ';', readFirstLine: true).collect { data ->
-    def duration = Date.parse('HH:mm', data[2])
-    duration.minutes = ((Math.floor((duration.minutes + 10) / 15) * 15).toInteger())
+def generateReport(rules, config) {
+    parseCsv(new File('report.csv').text, separator: ';', readFirstLine: true).collect { data ->
+        def duration = Date.parse('HH:mm', data[2])
+        duration.minutes = ((Math.floor((duration.minutes + 10) / 15) * 15).toInteger())
 
-    if (duration.hours == 0 && duration.minutes < 15) {
-        duration.minutes = 15
+        if (duration.hours == 0 && duration.minutes < 15) {
+            duration.minutes = 15
+        }
+
+        def rule = rules.find { data[1] =~ it.match }
+
+        if (!rule) {
+            throw new Exception('did not find mapping for "' + data[1] + '"')
+        }
+
+        def groupEntry = config.tasks.find { it.key =~ rule.group }
+        def task
+        if (groupEntry) {
+            task = groupEntry.value.find { it =~ rule.task }
+        }
+
+        if (!task || !groupEntry) {
+            throw new Exception('did not find mapping for rule: ' + groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(rule)))
+        }
+
+        return [data[0], duration.format('HH:mm'), rule.group, rule.task, rule.activity, rule.comment]
     }
-
-    def rule = rules.find { data[1] =~ it.match }
-
-    if (!rule) {
-        println 'did not find mapping for "' + data[1] + '"'
-        return
-    }
-
-    def groupEntry = config.tasks.find { it.key =~ rule.group }
-    def task
-    if (groupEntry) {
-        task = groupEntry.value.find { it =~ rule.task }
-    }
-
-    if (!task || !groupEntry) {
-        println 'did not find mapping for rule: '
-        println groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(rule))
-        return
-    }
-
-    return [data[0], duration.format('HH:mm'), rule.group, rule.task, rule.activity, rule.comment]
 }
 
-if (!report.findAll({ el -> el == null }).empty) {
-    System.exit 1
-}
+def rules = new groovy.json.JsonSlurper().parseText new File('rules.json').text
+def config = new groovy.json.JsonSlurper().parseText(new File('config.json').text)
 
-new File('report.csv').withWriter { writer ->
-    writer.writeLine report.collect { it.join(';') }.join('\n')
+try {
+    def report = generateReport(rules, config)
+
+    new File('report.csv').withWriter { writer ->
+        writer.writeLine report.collect { it.join(';') }.join('\n')
+    }
+} catch (err) {
+    println err.message
 }
--- 03_aardvark_map_data_ui.groovy
+++ 03_aardvark_map_data_ui.groovy
@@ -6,13 +6,48 @@ import static java.awt.EventQueue.invokeLater
 def rules = new groovy.json.JsonSlurper().parseText(new File('rules.json').text)
 def config = new groovy.json.JsonSlurper().parseText(new File('config.json').text)
 
+GroovyShell shell = new GroovyShell()
+def tools = shell.parse(new File('03_aardvark_map_data.groovy'))
+
 new SwingBuilder().edt {
   frame(title: 'Java Frame', size: [1000, 600], locationRelativeTo: null, show: true, defaultCloseOperation: WindowConstants.EXIT_ON_CLOSE) {
     menuBar {
-      menuItem(id:'scripts', text: 'Add Rule', actionPerformed: { 
-        rules.add([:])
-        rulesTable.model.fireTableDataChanged()
-        rulesTable.setRowSelectionInterval(rules.size() - 1, rules.size() - 1)
+      menu(text: 'Rules') {
+        menuItem(text: 'Add Rule', actionPerformed: { 
+          rules.add([:])
+          rulesTable.model.fireTableDataChanged()
+          rulesTable.setRowSelectionInterval(rules.size() - 1, rules.size() - 1)
+        })
+        menuItem(text: 'Remove Rule', enabled: bind { rulesTable.selectedElement }, actionPerformed: { 
+          rules.remove(rulesTable.selectedElement)
+          rulesTable.model.fireTableDataChanged()
+        })
+        menuItem(text: 'Save Rules', actionPerformed: {
+
+          new File('rules.json').withWriter { writer->
+              writer.writeLine groovy.json.JsonOutput.prettyPrint(groovy.json.JsonOutput.toJson(rules))
+          }
+        })
+      }
+      menuItem(text: 'Generate Report', actionPerformed: { 
+        tabs.selectedIndex = 1
+        try {
+          output.text = tools.generateReport(rules, config).join '\n'
+        } catch (err) {
+          output.text= err.message
+        }
+      })
+      menuItem(text: 'Write Report', actionPerformed: { 
+        tabs.selectedIndex = 1
+        try {
+          def report = tools.generateReport(rules, config).collect { it.join ';' }.join '\n'
+          output.text = report
+          new File('report.csv').withWriter { writer ->
+              writer.writeLine report
+          }
+        } catch (err) {
+          output.text= err.message
+        }
       })
     }  
 
@@ -30,44 +65,56 @@ new SwingBuilder().edt {
             closureColumn(header:'comment', read:{row -> return row.comment})
           }
         }
+        rulesTable.selectionModel.addListSelectionListener { 
+          if (rulesTable.selectedElement != null)
+            tabs.selectedIndex = 0
+        }
       }
     }
 
-    def selectedRule = { rules.find({ it.is(rulesTable.selectedElement) }) }
+    tabbedPane(id: 'tabs', tabLayoutPolicy:JTabbedPane.SCROLL_TAB_LAYOUT) {
 
-    panel(visible: bind { rulesTable.selectedElement != null }) {
-      gridLayout(cols: 2, rows: 5)
-      
-      label(text: 'Match: ', horizontalAlignment: SwingConstants.RIGHT)
-      matchInput = textField(text: bind { rulesTable.selectedElement?.match })
-      matchInput.addCaretListener { selectedRule()?.match = matchInput.text }
+      def selectedRule = { rules.find({ it.is(rulesTable.selectedElement) }) }
 
-      def getGroups = { config.tasks.keySet() as List }
-      label(text: 'Group: ', horizontalAlignment: SwingConstants.RIGHT)
-      groupSelect = comboBox(items: getGroups(), selectedItem: bind { rulesTable.selectedElement?.group })
-      groupSelect.addItemListener { selectedRule()?.group = groupSelect.selectedItem }
-      
-      label(text: 'Task: ', horizontalAlignment: SwingConstants.RIGHT)
-      taskSelect = comboBox()
-      def taskSelectUpdate = { 
-        def list = config.tasks.find { key, value -> key =~ groupSelect.selectedItem }?.value ?: []
-        taskSelect.model = new DefaultComboBoxModel(list.toArray())
-        taskSelect.model.selectedItem = list.find { it =~ rulesTable.selectedElement?.task }
+      panel(name: 'Rule Edit', visible: bind { rulesTable.selectedElement != null }) {
+        gridLayout(cols: 2, rows: 5)
+        
+        label(text: 'Match: ', horizontalAlignment: SwingConstants.RIGHT)
+        matchInput = textField(text: bind { rulesTable.selectedElement?.match })
+        matchInput.addCaretListener { selectedRule()?.match = matchInput.text }
+
+        def getGroups = { config.tasks.keySet() as List }
+        label(text: 'Group: ', horizontalAlignment: SwingConstants.RIGHT)
+        groupSelect = comboBox(items: getGroups(), selectedItem: bind { rulesTable.selectedElement?.group })
+        groupSelect.addItemListener { selectedRule()?.group = groupSelect.selectedItem }
+        
+        label(text: 'Task: ', horizontalAlignment: SwingConstants.RIGHT)
+        taskSelect = comboBox()
+        def taskSelectUpdate = { 
+          def list = config.tasks.find { key, value -> key =~ groupSelect.selectedItem }?.value ?: []
+          taskSelect.model = new DefaultComboBoxModel(list.toArray())
+          taskSelect.model.selectedItem = list.find { it =~ rulesTable.selectedElement?.task }
+        }
+        groupSelect.addItemListener taskSelectUpdate
+        rulesTable.selectionModel.addListSelectionListener taskSelectUpdate
+        taskSelect.addItemListener { 
+          if (taskSelect.selectedItem != null)
+            selectedRule().task = taskSelect.selectedItem
+        }
+        
+        label(text: 'Activity: ', horizontalAlignment: SwingConstants.RIGHT)
+        activitySelect = comboBox(items: config.activities, selectedItem: bind { rulesTable.selectedElement?.activity })
+        activitySelect.addItemListener { selectedRule()?.activity = activitySelect.selectedItem }
+        
+        label(text: 'Comment: ', horizontalAlignment: SwingConstants.RIGHT)
+        commentInput = textField(text: bind { rulesTable.selectedElement?.comment }, actionPerformed: { invokeLater { rulesTable.model.fireTableDataChanged() } })
+        commentInput.addCaretListener { selectedRule()?.comment = commentInput.text }
       }
-      groupSelect.addItemListener taskSelectUpdate
-      rulesTable.selectionModel.addListSelectionListener taskSelectUpdate
-      taskSelect.addItemListener { 
-        if (taskSelect.selectedItem != null)
-          selectedRule().task = taskSelect.selectedItem
+
+      panel(name: 'Output') {
+        borderLayout()
+        textArea(id: 'output', editable: false, alignmentY: SwingConstants.TOP)
       }
-      
-      label(text: 'Activity: ', horizontalAlignment: SwingConstants.RIGHT)
-      activitySelect = comboBox(items: config.activities, selectedItem: bind { rulesTable.selectedElement?.activity })
-      activitySelect.addItemListener { selectedRule()?.activity = activitySelect.selectedItem }
-      
-      label(text: 'Comment: ', horizontalAlignment: SwingConstants.RIGHT)
-      commentInput = textField(text: bind { rulesTable.selectedElement?.comment })
-      commentInput.addCaretListener { selectedRule()?.comment = commentInput.text }
     }
   }
 }
